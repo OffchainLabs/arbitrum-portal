0e5bbe929536d3cdb965c16c3f052e63f0adedc3
Fionna <13184582+fionnachan@users.noreply.github.com>
2025-09-17 16:11:15 +0100

    services list

--- packages/arb-token-bridge-ui/src/components/BuyPanel/Homepage.tsx
+++ packages/arb-token-bridge-ui/src/components/BuyPanel/Homepage.tsx
@@ -1,5 +1,15 @@
 import { PropsWithChildren } from 'react'
 import Image from 'next/image'
+import { ChainId } from '@/bridge/types/ChainId'
+
+const onrampServices = [
+  {
+    name: 'MoonPay',
+    logo: '/images/onramp-logos/moonpay-logo.png',
+    link: 'https://www.moonpay.com/buy',
+    chains: [ChainId.Ethereum, ChainId.ArbitrumOne]
+  }
+] as const
 
 function OnrampServiceTile({
   name,
