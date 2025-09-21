8660d5eb5dea9a8ac093f19170ceec2ed92993e2
Christophe <contact@christophedeveaux.com>
2025-09-21 18:59:40 +0000

    Sanitize in embed mode

--- packages/app/src/app/(embed)/bridge/embed/page.tsx
+++ packages/app/src/app/(embed)/bridge/embed/page.tsx
@@ -2,8 +2,21 @@ import BridgeClient from '../../../(with-sidebar)/bridge/BridgeClient'
 import { Toast } from '@/bridge/components/common/atoms/Toast'
 
 import '../../../../styles/bridge.css'
+import { addOrbitChainsToArbitrumSDK } from 'packages/app/src/initialization'
+import { sanitizeAndRedirect } from 'packages/app/src/utils/sanitizeAndRedirect'
 
-export default async function EmbededPage() {
+export default async function EmbededPage({
+  searchParams
+}: {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}) {
+  if (searchParams.sanitized !== 'true') {
+    addOrbitChainsToArbitrumSDK()
+    await sanitizeAndRedirect(searchParams)
+  }
+
   return (
     <>
       <BridgeClient />
