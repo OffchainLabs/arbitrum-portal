2065c648b7d65cce8606caf06881b60569dca636
Christophe <contact@christophedeveaux.com>
2025-09-23 10:23:39 +0000

    Load external CSS for embeded bridge

--- packages/app/src/app/(embed)/bridge/embed/layout.tsx
+++ packages/app/src/app/(embed)/bridge/embed/layout.tsx
@@ -1,5 +1,10 @@
 import { PropsWithChildren } from 'react'
 
+import 'tippy.js/dist/tippy.css'
+import 'tippy.js/themes/light.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import 'react-toastify/dist/ReactToastify.css'
+
 export default function EmbedLayout(props: PropsWithChildren) {
   return <div className="bg-widget-background">{props.children}</div>
 }
