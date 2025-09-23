a07a61a01958e78cf88eabb1cbf5637fa9522172
Fionna <13184582+fionnachan@users.noreply.github.com>
2025-09-23 19:09:48 +0100

    add prefix .portal

--- packages/portal/components/PortalPage/PortalPage.tsx
+++ packages/portal/components/PortalPage/PortalPage.tsx
@@ -3,7 +3,7 @@ import { SitewideSearchBar } from './SitewideSearchBar';
 
 export const PortalPage = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="portal">
       <div
         id="sticky-top-bar"
         className="sticky top-0 z-40 flex w-full flex-col items-center gap-4 py-6 pb-4 transition-all duration-300 lg:pb-6"
@@ -18,6 +18,6 @@ export const PortalPage = ({ children }: { children: React.ReactNode }) => {
       <div className="relative mx-auto flex w-full max-w-[1153px] flex-col gap-4 px-6 pb-[160px] lg:gap-6">
         {children}
       </div>
-    </>
+    </div>
   );
 };
--- packages/portal/postcss.config.js
+++ packages/portal/postcss.config.js
@@ -2,5 +2,9 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'tailwindcss/nesting': {},
+    'postcss-prefix-selector': {
+      prefix: '.portal',
+    },
   },
-}
+};
