f6ad9146292785e9d593140196512d0b76bea35e
Christophe <contact@christophedeveaux.com>
2025-09-24 08:53:38 +0000

    Remove test

--- packages/app/src/app/layout.tsx
+++ packages/app/src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   }
 }
 
-export default function RootLayout({ children, a }: PropsWithChildren) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={twMerge('relative bg-black text-white', unica.variable)}>
