b2d8cf7452c4e62623498833c7a9a1977b50fef0
Christophe <contact@christophedeveaux.com>
2025-10-06 11:37:18 +0000

    Remove sidebar changes

--- packages/portal/components/AppSidebar.tsx
+++ packages/portal/components/AppSidebar.tsx
@@ -1,45 +1,57 @@
 'use client';
 
 import { Sidebar } from '@offchainlabs/cobalt';
-import { useSearchParams, useSelectedLayoutSegments } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { usePostHog } from 'posthog-js/react';
-import { useState } from 'react';
-
-function getActiveMenu(segment: string | undefined) {
-  switch (segment) {
-    case 'bridge':
-      return 'Bridge';
-    case '(portal)':
-      return 'Home';
-    case 'defi':
-      return 'DeFi';
-    case 'ai-and-depin':
-      return 'AI & DePIN';
-    case 'bridges-and-on-ramps':
-      return 'Bridges & On-ramps';
-    case 'gaming':
-      return 'Gaming';
-    case 'nfts':
-      return 'NFTs';
-    case 'infra-and-tools':
-      return 'Infra & Tools';
-    case 'arcade':
-      return 'Arcade';
-    case 'ecosystem':
-      return 'Chain Navigator';
-    case 'metrics':
-      return 'Chain Stats';
-    default:
-      return '';
-  }
-}
+import { useEffect, useState } from 'react';
 
 export const AppSidebar = () => {
   const posthog = usePostHog();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
-  const segments = useSelectedLayoutSegments();
 
-  const [activeMenu, setActiveMenu] = useState(getActiveMenu(segments[segments.length - 1]));
+  useEffect(() => {
+    switch (pathname) {
+      case '/bridge':
+        setActiveMenu('Bridge');
+        break;
+      case '/':
+        setActiveMenu('Home');
+        break;
+      case '/projects/defi':
+        setActiveMenu('DeFi');
+        break;
+      case '/projects/ai-and-depin':
+        setActiveMenu('AI & DePIN');
+        break;
+      case '/projects/bridges-and-on-ramps':
+        setActiveMenu('Bridges & On-ramps');
+        break;
+      case '/projects/gaming':
+        setActiveMenu('Gaming');
+        break;
+      case '/projects/nfts':
+        setActiveMenu('NFTs');
+        break;
+      case '/projects/infra-and-tools':
+        setActiveMenu('Infra & Tools');
+        break;
+      case '/arcade':
+        setActiveMenu('Arcade');
+        break;
+      case '/chains/ecosystem':
+        setActiveMenu('Ecosystem');
+        break;
+      case '/chains/metrics':
+        setActiveMenu('Metrics');
+        break;
+      default:
+        setActiveMenu('');
+        break;
+    }
+  }, [pathname]);
+
+  const [activeMenu, setActiveMenu] = useState('Home');
   const chainsParam = searchParams.get('chains') || '';
   const linkPostfix = chainsParam && `?chains=${encodeURIComponent(chainsParam)}`;
 
