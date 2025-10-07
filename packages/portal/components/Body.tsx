66491546c352c31c41f60dc2d97452c4a57f6860
Christophe Deveaux <contact@christophedeveaux.com>
2025-10-07 13:11:18 +0200

    chore(portal): remove unused code (#78)

--- packages/portal/components/Body.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Body = ({ children }: { children: React.ReactNode }) => {
-  return <div className="relative z-[5] min-h-[calc(100vh-434px)] py-12">{children}</div>;
-};
