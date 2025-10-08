b0d8571f90ac82da53f779a0e5480795c65c0c99
Dewansh Parashar <dparashar@offchainlabs.com>
2025-10-08 15:26:32 +0400

    dev: added zindex

--- packages/arb-token-bridge-ui/src/components/Widget/WidgetTransferPanel.tsx
+++ packages/arb-token-bridge-ui/src/components/Widget/WidgetTransferPanel.tsx
@@ -60,7 +60,7 @@ export function WidgetTransferPanel({
               <WidgetRoutes />
             </div>
 
-            <div className="flex flex-col gap-2 max-[850px]:fixed max-[850px]:left-0 max-[850px]:bottom-0 max-[850px]:w-full max-[850px]:p-4 max-[850px]:bg-widget-background">
+            <div className="flex flex-col gap-2 max-[850px]:fixed max-[850px]:left-0 max-[850px]:bottom-0 max-[850px]:w-full max-[850px]:p-4 max-[850px]:bg-widget-background z-10">
               <ToSConfirmationCheckbox />
 
               {isConnected ? (
