aeebd6f34776cd7ed2aca716376d649fe0429e46
Dewansh <dparashar@offchainlabs.com>
2025-11-10 17:03:07 +0530

    chore: add Muster in withdraw-only mode (#109)

--- packages/arb-token-bridge-ui/src/components/TransferPanel/useTransferReadiness.ts
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/useTransferReadiness.ts
@@ -41,7 +41,7 @@ import {
 export const DISABLED_CHAIN_IDS: number[] = [];
 
 // withdraw-only chains (will also display error message in the transfer panel)
-const WITHDRAW_ONLY_CHAIN_IDS: number[] = [];
+const WITHDRAW_ONLY_CHAIN_IDS: number[] = [4078];
 
 type ErrorMessages = {
   inputAmount1?: string | TransferReadinessRichErrorMessage;
