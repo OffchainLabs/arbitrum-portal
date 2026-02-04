cad5469b4a70c537979abb1ad575ec37599257e6
dewanshparashar <dparashar@offchainlabs.com>
2026-02-05 02:17:20 +0400

    dev: footer

--- packages/portal/components/SocialsFooter.tsx
+++ packages/portal/components/SocialsFooter.tsx
@@ -1,13 +1,21 @@
-import { DISCORD_LINK } from '@/common/constants';
+import {
+  ARBITRUM_TECHNOLOGY_LINK,
+  DISCORD_LINK,
+  MEDIA_KIT_LINK,
+  OFFCHAIN_LABS_LINK,
+  PRIVACY_POLICY_LINK,
+  STATUS_LINK,
+  TERMS_OF_SERVICE_LINK,
+} from '@/common/constants';
 
 import { ExternalLink } from './ExternalLink';
 import { DiscordIcon, GithubIcon, TwitterXIcon } from './SvgIcons';
 
 export const SocialsFooter = () => {
   return (
-    <div className="mx-auto w-full max-w-[1153px] px-6 pb-6">
+    <div className="mx-auto w-full max-w-[1153px] px-6 pb-6 mb-[100px]">
       <hr className="mb-6 border-2 border-white/50" />
-      <div className="flex flex-nowrap items-center justify-between gap-4 text-xl">
+      <div className="mb-6 flex flex-nowrap items-center justify-between gap-4 text-xl">
         Get Connected
         <div className="flex gap-4">
           <ExternalLink
@@ -29,6 +37,36 @@ export const SocialsFooter = () => {
             href="https://github.com/OffchainLabs"
           >
             <GithubIcon size={20} />
+          </ExternalLink>
+        </div>
+      </div>
+      <div className="flex flex-wrap items-center justify-between gap-4 text-sm">
+        <div>
+          Built with love by{' '}
+          <ExternalLink className="underline hover:no-underline" href={OFFCHAIN_LABS_LINK}>
+            Offchain Labs
+          </ExternalLink>
+          , builders of{' '}
+          <ExternalLink className="underline hover:no-underline" href={ARBITRUM_TECHNOLOGY_LINK}>
+            Arbitrum technology
+          </ExternalLink>
+          .
+        </div>
+        <div className="flex items-center gap-2">
+          <ExternalLink className="underline hover:no-underline" href={STATUS_LINK}>
+            Status
+          </ExternalLink>
+          <span className="text-white/50">•</span>
+          <ExternalLink className="underline hover:no-underline" href={TERMS_OF_SERVICE_LINK}>
+            ToS
+          </ExternalLink>
+          <span className="text-white/50">•</span>
+          <ExternalLink className="underline hover:no-underline" href={PRIVACY_POLICY_LINK}>
+            Privacy Policy
+          </ExternalLink>
+          <span className="text-white/50">•</span>
+          <ExternalLink className="underline hover:no-underline" href={MEDIA_KIT_LINK}>
+            Media Kit
           </ExternalLink>
         </div>
       </div>
