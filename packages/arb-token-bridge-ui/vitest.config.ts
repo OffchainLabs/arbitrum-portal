354713de7d8676994e9701ead242643203e79ba8
Christophe Deveaux <contact@christophedeveaux.com>
2026-02-10 23:36:33 +0100

    fix(monitoring): update tsconfig paths (#154)

--- packages/arb-token-bridge-ui/vitest.config.ts
+++ packages/arb-token-bridge-ui/vitest.config.ts
@@ -19,6 +19,8 @@ export default defineConfig({
     alias: {
       '@/images': path.resolve(__dirname, '../app/public/images'),
       '@/icons': path.resolve(__dirname, '../app/public/icons'),
+      '@/common': path.resolve(__dirname, '../portal/common'),
+      '@/portal': path.resolve(__dirname, '../portal'),
       '@/token-bridge-sdk': path.resolve(__dirname, './src/token-bridge-sdk'),
       '@/bridge': path.resolve(__dirname, './src'),
     },
