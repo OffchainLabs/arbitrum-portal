270bd0c6c062a575910c5913d568e665bc2dec1d
dewanshparashar <dparashar@offchainlabs.com>
2026-02-12 23:24:36 +0530

    dev: fix

--- packages/app/src/components/AppShell/components/PageHeading.tsx
+++ packages/app/src/components/AppShell/components/PageHeading.tsx
@@ -2,8 +2,6 @@ import { PropsWithChildren } from 'react';
 
 export function PageHeading({ children }: PropsWithChildren) {
   return (
-    <h1 className="hidden md:block text-[32px] font-bold text-white px-0 m-0 leading-[32px]">
-      {children}
-    </h1>
+    <h1 className="hidden md:block text-3xl font-bold text-white px-0 m-0 leading-8">{children}</h1>
   );
 }
