419a8da2f49ff6a1f847a6906fc0c0b41098bf83
dewanshparashar <dparashar@offchainlabs.com>
2026-07-30 22:31:33 +0530

    fix(bridge): reword Trust Wallet warning dialog

--- packages/arb-token-bridge-ui/src/components/TransferPanel/TrustWalletUpdateDialog.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TrustWalletUpdateDialog.tsx
@@ -4,15 +4,20 @@ export function TrustWalletUpdateDialog(props: DialogProps) {
   return (
     <Dialog
       {...props}
-      title="Update Trust Wallet"
-      actionButtonTitle="Confirm"
+      title="Warning: Trust Wallet"
+      actionButtonTitle="Proceed"
       className="md:max-w-[480px]"
     >
-      <p className="py-4 text-sm font-light">
-        Old versions of Trust Wallet can sign on the wrong network and lose funds. Please ensure you
-        are on the latest version of the mobile app or browser extension, and that you sign on the
-        correct network.
-      </p>
+      <div className="flex flex-col gap-3 py-4 text-sm font-light">
+        <p>We detected you&apos;re connected via Trust Wallet.</p>
+
+        <p>Old versions of Trust Wallet can sign on the wrong network and lose funds.</p>
+
+        <p>
+          Please ensure you are on the latest version of the mobile app or browser extension, and
+          that you sign on the correct network.
+        </p>
+      </div>
     </Dialog>
   );
 }
